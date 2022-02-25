6a0aa92c35ca1e4282655b9d57a95bca939a96a9
DenysRogeres <dennysrogeres@gmail.com>
2022-02-25 01:47:06 -0300

    criando botao de concluir e remover e adicionando tarefas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from './models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-angular';
+  public title: string = "Hello World";
+  public todos: Todo[] = [];
+
+  constructor() {
+    this.todos.push({id: 1, title: "Cortar cabelo", done: true})
+    this.todos.push({id: 2, title: "Passear com cachorro", done: false})
+    this.todos.push({id: 3, title: "Estudar para prova", done: false})
+    this.todos.push({id: 4, title: "Limpar casa", done: false})
+  }
+
+  alterarTexto() {
+    this.title = "New title";
+  }
 }
