346d0164ea6782fca4e7c4a893621db51d0638ed
DenysRogeres <dennysrogeres@gmail.com>
2022-03-01 02:02:13 -0300

    persistindo dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,6 +20,7 @@ export class AppComponent {
         Validators.required
       ])]
     })
+    this.load();
   }
 
   add() {
@@ -34,10 +35,6 @@ export class AppComponent {
     this.form.reset;
   }
 
-  save() {
-    const data = JSON.stringify(this.todos);
-    localStorage.setItem('todos', data);
-  }
 
   alterarTexto() {
     this.title = "New title";
@@ -49,14 +46,27 @@ export class AppComponent {
     if(index != -1) {
       this.todos.splice(index, 1);
     }
+    this.save();
   }
 
   markAsDone(todo: Todo) {
     todo.done = true;
+    this.save();
   }
 
   markAsUndone(todo: Todo) {
     todo.done = false;
+    this.save();
+  }
+
+  save() {
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+  }
+
+  load() {
+    const data = localStorage.getItem('todos');
+    this.todos = JSON.parse(data || '{}');
   }
 
 }
